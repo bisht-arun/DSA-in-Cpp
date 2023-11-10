883f620bba073d6c584ec36eba512f92efd21b10
bisht-arun <arunbisht402@gmail.com>
2023-11-10 12:26:58 +0530

    ok

--- Lecture 6 - OOPS/template.cpp
+++ Lecture 6 - OOPS/template.cpp
@@ -26,3 +26,16 @@ public:
         return y;
     }
 };
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Pair<int> p1;
+
+    p1.setX(10);
+    p1.setY(20);
+
+    cout << p1.getX() << " " << p1.getY() << endl;
+}
