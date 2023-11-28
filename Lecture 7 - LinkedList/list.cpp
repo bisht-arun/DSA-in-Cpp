bdffd7c4674c901bcfe081a7baf14cb1f3b38fc4
bisht-arun <arunbisht402@gmail.com>
2023-11-28 12:31:17 +0530

    added

--- Lecture 7 - LinkedList/list.cpp
+++ Lecture 7 - LinkedList/list.cpp
@@ -14,22 +14,49 @@ public:
     }
 };
 
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
 int main()
 {
-    Node n1(1);
-    Node n2(2);
-    Node n3(3);
-    Node n4(4);
-    Node n5(5);
+    /*
+        //Statically making of Linked List
+        Node n1(1);
+        Node n2(2);
+        Node n3(3);
+        Node n4(4);
+        Node n5(5);
 
-    n1.next = &n2;
-    n2.next = &n3;
-    n3.next = &n4;
-    n4.next = &n5;
+        n1.next = &n2;
+        n2.next = &n3;
+        n3.next = &n4;
+        n4.next = &n5;
 
-    cout << n1.data << endl;
-    cout << n2.data << endl;
-    cout << n3.data << endl;
-    cout << n4.data << endl;
-    cout << n5.data << endl;
+        cout << n1.data << endl;
+        cout << n2.data << endl;
+        cout << n3.data << endl;
+        cout << n4.data << endl;
+        cout << n5.data << endl;
+    */
+
+    // Dynamically making of Linked List
+    Node *n1 = new Node(10);
+    Node *head = n1;
+    Node *n2 = new Node(20);
+    Node *n3 = new Node(30);
+    Node *n4 = new Node(40);
+    Node *n5 = new Node(50);
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+
+    print(head);
 }
